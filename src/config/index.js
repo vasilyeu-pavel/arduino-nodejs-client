2a4b219047c8d8f720e98f2584722ced0577efd0
Pavel Vasilyeu <pavel_vasilyeu@senla.eu>
2019-12-23 14:37:31 +0300

    change name

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ export default {
         socketUrl: 'ws://localhost:3000'
     },
     production: {
-        url: 'https://arduino-websocket-server.herokuapp.com/',
+        url: 'https://arduino-websocket-server.herokuapp.com',
         host: 'powerful-falls-48878.herokuapp.com',
         socketUrl: 'wss://arduino-websocket-server.herokuapp.com'
     }
